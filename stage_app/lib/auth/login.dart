4e23f98ff27b2ab04f0bf187a0841cee2f43890a
ahmedsalem <22050@supnum.mr>
2024-06-03 17:37:57 +0000

    first commit

---
+++ stage_app/lib/auth/login.dart
@@ -0,0 +1,259 @@
+// ignore_for_file: prefer_const_constructors, use_build_context_synchronously, use_key_in_widget_constructors, library_private_types_in_public_api, deprecated_member_use, unused_element
+
+import 'package:flutter/material.dart';
+import '../data/mongo_database.dart';
+import '../screens/home_page.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> with TickerProviderStateMixin {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isLoading = false;
+  bool _obscurePassword = true;
+
+  Future<void> _login() async {
+    final email = _emailController.text;
+    final password = _passwordController.text;
+
+    if (email.isEmpty || password.isEmpty) {
+      _showSnackBar('Veuillez remplir tous les champs.');
+      return;
+    }
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final authenticated = await MongoDatabase.login(email, password);
+
+      setState(() {
+        _isLoading = false;
+      });
+
+      if (authenticated) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+      } else {
+        _showSnackBar('Email ou mot de passe incorrect.');
+      }
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+      _showSnackBar('Une erreur est survenue. Veuillez réessayer.');
+    }
+  }
+
+  void _showSnackBar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade800,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Stack(
+        children: [
+          //Animated Gradient Background
+          AnimatedContainer(
+            duration: Duration(seconds: 7),
+            onEnd: () {
+              setState(() {});
+            },
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.blue.shade800, Colors.purple.shade600],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+          ),
+          // Main Content
+          Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
+              child: AnimatedSwitcher(
+                duration: Duration(milliseconds: 500),
+                child: Card(
+                  key: ValueKey<int>(1),
+                  elevation: 15,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  shadowColor: Colors.grey.withOpacity(0.7),
+                  child: Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Connexion',
+                          style: theme.textTheme.headlineMedium?.copyWith(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87,
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        _buildTextField(
+                          controller: _emailController,
+                          labelText: 'Email',
+                          icon: Icons.email,
+                        ),
+                        SizedBox(height: 16),
+                        _buildTextField(
+                          controller: _passwordController,
+                          labelText: 'Mot de passe',
+                          icon: Icons.lock,
+                          obscureText: _obscurePassword,
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _obscurePassword
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Colors.blue,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _obscurePassword = !_obscurePassword;
+                              });
+                            },
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        AnimatedSwitcher(
+                          duration: Duration(milliseconds: 300),
+                          child: _isLoading
+                              ? CircularProgressIndicator(
+                                  key: ValueKey<int>(1),
+                                  valueColor: AlwaysStoppedAnimation<Color>(
+                                      Colors.blue),
+                                )
+                              : _buildLoginButton(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          if (_isLoading)
+            Opacity(
+              opacity: 0.8,
+              child: ModalBarrier(dismissible: false, color: Colors.black),
+            ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String labelText,
+    required IconData icon,
+    bool obscureText = false,
+    Widget? suffixIcon,
+  }) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 8,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          prefixIcon: Icon(icon, color: Colors.blue),
+          suffixIcon: suffixIcon,
+          labelText: labelText,
+          labelStyle: TextStyle(color: Colors.blue),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue, width: 2.0),
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+        ),
+        obscureText: obscureText,
+      ),
+    );
+  }
+
+  Widget _buildLoginButton() {
+    return ElevatedButton(
+      key: ValueKey<int>(2),
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.blue,
+        padding: EdgeInsets.symmetric(
+          vertical: 16.0,
+          horizontal: 32.0,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25.0),
+        ),
+        textStyle: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+        elevation: 10,
+        shadowColor: Colors.blueAccent,
+      ).copyWith(
+        elevation: MaterialStateProperty.resolveWith<double>(
+          (Set<MaterialState> states) {
+            if (states.contains(MaterialState.pressed)) {
+              return 15;
+            }
+            return 10; // Default elevation
+          },
+        ),
+      ),
+      onPressed: _login,
+      child: Text('Se Connecter'),
+    );
+  }
+
+  Widget _buildTextButton(
+      {required String label, required VoidCallback onPressed}) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        foregroundColor: Colors.blue,
+        textStyle: TextStyle(
+          decoration: TextDecoration.underline,
+          fontSize: 16,
+        ),
+      ),
+      onPressed: onPressed,
+      child: Text(label),
+    );
+  }
+}
